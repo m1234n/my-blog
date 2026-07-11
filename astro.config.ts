b2c4d6d16fda7361288c0f6cb81ad47fa457e124
m1234n <tkanglin@163.com>
2026-07-11 21:48:24 +0800

    github action

--- astro.config.ts
+++ astro.config.ts
@@ -22,6 +22,7 @@ import { expressiveCodeOptions, siteConfig } from "./src/site.config";
 // https://astro.build/config
 export default defineConfig({
 	site: siteConfig.url,
+	base: "/my-blog",
 	image: {
 		domains: ["webmention.io"],
 	},
